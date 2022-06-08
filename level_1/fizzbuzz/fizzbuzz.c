435bbae7e958c8fdb0b521a26c47f0e020738b8a
nwesche <95024094+nwesche@users.noreply.github.com>
2022-06-08 14:15:39 +0200

    Update fizzbuzz.c

--- level_1/fizzbuzz/fizzbuzz.c
+++ level_1/fizzbuzz/fizzbuzz.c
@@ -12,107 +12,29 @@
 
 #include <unistd.h>
 
-int		main(void)
+void	ft_write_number(int number)
 {
-	write(1, "1\n", 2);
-	write(1, "2\n", 2);
-	write(1, "fizz\n", 5);
-	write(1, "4\n", 2);
-	write(1, "buzz\n", 5);
-	write(1, "fizz\n", 5);
-	write(1, "7\n", 2);
-	write(1, "8\n", 2);
-	write(1, "fizz\n", 5);
-	write(1, "buzz\n", 5);
-	write(1, "11\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "13\n", 3);
-	write(1, "14\n", 3);
-	write(1, "fizzbuzz\n", 9);
-	write(1, "16\n", 3);
-	write(1, "17\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "19\n", 3);
-	write(1, "buzz\n", 5);
-	write(1, "fizz\n", 5);
-	write(1, "22\n", 3);
-	write(1, "23\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "buzz\n", 5);
-	write(1, "26\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "28\n", 3);
-	write(1, "29\n", 3);
-	write(1, "fizzbuzz\n", 9);
-	write(1, "31\n", 3);
-	write(1, "32\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "34\n", 3);
-	write(1, "buzz\n", 5);
-	write(1, "fizz\n", 5);
-	write(1, "37\n", 3);
-	write(1, "38\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "buzz\n", 5);
-	write(1, "41\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "43\n", 3);
-	write(1, "44\n", 3);
-	write(1, "fizzbuzz\n", 9);
-	write(1, "46\n", 3);
-	write(1, "47\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "49\n", 3);
-	write(1, "buzz\n", 5);
-	write(1, "fizz\n", 5);
-	write(1, "52\n", 3);
-	write(1, "53\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "buzz\n", 5);
-	write(1, "56\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "58\n", 3);
-	write(1, "59\n", 3);
-	write(1, "fizzbuzz\n", 9);
-	write(1, "61\n", 3);
-	write(1, "62\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "64\n", 3);
-	write(1, "buzz\n", 5);
-	write(1, "fizz\n", 5);
-	write(1, "67\n", 3);
-	write(1, "68\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "buzz\n", 5);
-	write(1, "71\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "73\n", 3);
-	write(1, "74\n", 3);
-	write(1, "fizzbuzz\n", 9);
-	write(1, "76\n", 3);
-	write(1, "77\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "79\n", 3);
-	write(1, "buzz\n", 5);
-	write(1, "fizz\n", 5);
-	write(1, "82\n", 3);
-	write(1, "83\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "buzz\n", 5);
-	write(1, "86\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "88\n", 3);
-	write(1, "89\n", 3);
-	write(1, "fizzbuzz\n", 9);
-	write(1, "91\n", 3);
-	write(1, "92\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "94\n", 3);
-	write(1, "buzz\n", 5);
-	write(1, "fizz\n", 5);
-	write(1, "97\n", 3);
-	write(1, "98\n", 3);
-	write(1, "fizz\n", 5);
-	write(1, "buzz\n", 5);
-	return (0);
+	if (number > 9)
+		ft_write_number(number / 10);
+	write(1, &"0123456789"[number % 10], 1);
+}
+
+int	main(void)
+{
+	int	number;
+
+	number = 1;
+	while (number <= 100)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (number % 3 == 0)
+			write(1, "fizz", 4);
+		else if (number % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_write_number(number);
+		write(1, "\n", 1);
+		number++;
+	}
 }
