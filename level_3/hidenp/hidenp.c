fd00a280fbafb509ab479f8c80f95ee084ad5d8d
nwesche <95024094+nwesche@users.noreply.github.com>
2022-07-13 21:32:46 +0200

    Update hidenp.c

--- level_3/hidenp/hidenp.c
+++ level_3/hidenp/hidenp.c
@@ -14,12 +14,14 @@
 
 int		ft_strcmp(char *s1, char *s2)
 {
-	while (*s2 && (*s1 == *s2))
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
-		s1 += 1;
-		s2 += 1;
+		i++;
 	}
-	return (*s1 - *s2);
+	return (s1[i] - s2[i]);
 }
 
 int		main(int argc, char *argv[])
